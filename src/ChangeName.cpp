6e5c77c3df2de981f3816800582456c9756de864
dperini <perini.davide@dpsoftware.org>
2020-04-20 18:57:15 +0200

    bootsrap classes refactor

--- src/ChangeName.cpp
+++ src/ChangeName.cpp
@@ -1,11 +1,27 @@
 /*
- * Arduino Bootstrapper
- * DPsoftware (Davide Perini)
- * 
- * Utility classes for bootstrapping arduino projects with Wifi management, OTA upload management, memory management, MQTT and queue management.
- * 
- * MIT license
- */
+  ChangeName.cpp - Main Arduino File - Utility classes for bootstrapping arduino projects with Wifi management, 
+  OTA upload management, memory management, MQTT and queue management.
+
+  Copyright (C) 2020  Davide Perini
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+  this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+  copies of the Software, and to permit persons to whom the Software is 
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in 
+  all copies or substantial portions of the Software.
+  
+  You should have received a copy of the MIT License along with this program.  
+  If not, see <https://opensource.org/licenses/MIT/>.
+*/
+
+#include "Configuration.h"
+
+// Initialize the display
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET); 
 #include "ChangeName.h"
 
 void setup() {
