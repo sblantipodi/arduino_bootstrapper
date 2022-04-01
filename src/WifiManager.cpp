a70611d69cf6320ce33e577f039e1735a8706128
Davide Perini <perini.davide@dpsoftware.org>
2022-04-02 01:26:10 +0200

    improv wifi

--- src/WifiManager.cpp
+++ src/WifiManager.cpp
@@ -258,9 +258,7 @@ void WifiManager::launchWebServerForOTAConfig() {
 // Manage improv wifi
 void WifiManager::manageImprovWifi() {
 
-  if (Serial.available() > 0 && Serial.peek() == 'I') {
-    handleImprovPacket();
-  }
+  handleImprovPacket();
 
 }
 
@@ -744,7 +742,7 @@ void WifiManager::handleImprovPacket() {
   uint16_t packetByte = 0;
   uint8_t packetLen = 9;
   uint8_t checksum = 0;
-  uint8_t waitTime = 25;
+  uint8_t waitTime = 5;
   uint8_t rpcCommandType = 0;
   char rpcData[128];
   rpcData[0] = 0;
